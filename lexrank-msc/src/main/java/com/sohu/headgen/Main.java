d235c23846d257328e0e3f039b05f76b08b3f0b4
YIN MIAO <yinmiaothink@gmail.com>
2018-10-08 18:50:14 +0800

    commit

--- lexrank-msc/src/main/java/com/sohu/headgen/Main.java
+++ lexrank-msc/src/main/java/com/sohu/headgen/Main.java
@@ -63,29 +63,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Path folder = Paths.get(args[0]);
 
-        List<String> sentences = Arrays.asList(
-                "The wife of a former U.S. president Bill Clinton, Hillary Clinton, visited China last Monday.",
-                "Hillary Clinton wanted to visit China last month but postponed her plans till Monday last week.",
-                "Hillary Clinton paid a visit to the People Republic of China on Monday.",
-                "Last week the Secretary State Ms. Clinton visited Chinese officials.");
-
-        Collection<String> stopWords = Arrays.asList("a", "able", "about", "above", "after", "all", "also", "an",
-                "and", "any", "as", "ask", "at", "back", "bad", "be", "because", "beneath", "big", "but", "by",
-                "call", "can", "case", "child", "come", "company", "could", "day", "different", "do", "early", "even",
-                "eye", "fact", "feel", "few", "find", "first", "for", "from", "get", "give", "go", "good",
-                "government", "great", "group", "hand", "have", "he", "her", "high", "him", "his", "how", "i", "if",
-                "important", "in", "into", "it", "its", "just", "know", "large", "last", "leave", "life", "like",
-                "little", "long", "look", "make", "man", "me", "most", "my", "new", "next", "no", "not", "now",
-                "number", "of", "old", "on", "one", "only", "or", "other", "our", "out", "over", "own", "part",
-                "people", "person", "place", "point", "problem", "public", "right", "same", "say", "see", "seem",
-                "she", "small", "so", "some", "take", "tell", "than", "that", "the", "their", "them", "then", "there",
-                "these", "they", "thing", "think", "this", "time", "to", "try", "two", "under", "up", "us", "use",
-                "want", "way", "we", "week", "well", "what", "when", "which", "who", "will", "with", "woman", "work",
-                "world", "would", "year", "you", "young", "your");
-
         Summarizer summarizer = new Summarizer();
 
-        List<String> summary = summarizer.getSummary("256679269", 4, 0.1, 0.01);
+        List<String> summary = summarizer.getSummary("256859899", 4, 0.1, 0.01);
+
+        List<String> stopWords = new ArrayList<>();
 
         HeadlineGenerator generator = HeadlineGenerator.builder()
                 .on(folder)
